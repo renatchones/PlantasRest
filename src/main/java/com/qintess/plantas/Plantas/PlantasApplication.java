63b3cfe3077f0491b3ca167bbcb81f93815dc7ae
Renato Moraes <renato.moraes1@outlook.com>
2020-07-21 08:58:42 -0300

    iniciando o git hub

---
+++ src/main/java/com/qintess/plantas/Plantas/PlantasApplication.java
@@ -0,0 +1,13 @@
+package com.qintess.plantas.Plantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qintess/plantas/Plantas/PlantasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.plantas.Plantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
